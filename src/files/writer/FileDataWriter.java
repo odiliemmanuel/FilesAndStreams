96120e0b912607db54c070a50463bf068eb0ed6f
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-06 11:24:40 +0100

    "files"

--- src/files/writer/FileDataWriter.java
+++ src/files/writer/FileDataWriter.java
@@ -78,7 +78,7 @@ public class FileDataWriter {
         return value.length();
     }
 
-    tring text, String fileLocation){
+    ing text, String fileLocation){
         try (FileOutputStream outputStream = new FileOutputStream(fileLocation)) {
 
         }catch (IOException e){
