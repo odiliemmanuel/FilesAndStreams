7246e6dd492537a36d8a9813fef9cf26c2457565
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-06 11:21:51 +0100

    "files"

--- src/files/writer/FileDataWriter.java
+++ src/files/writer/FileDataWriter.java
@@ -78,7 +78,7 @@ public class FileDataWriter {
         return value.length();
     }
 
-    public static void writeDataToFile(String text, String fileLocation){
+    ublic static void writeDataToFile(String text, String fileLocation){
         try (FileOutputStream outputStream = new FileOutputStream(fileLocation)) {
 
         }catch (IOException e){
